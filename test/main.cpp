0f35ee957a7017cf0e81cdab259eb71732b1b6d3
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-01-12 19:59:04 +0300

    запустил тесты для net

--- test/main.cpp
+++ test/main.cpp
@@ -2,14 +2,16 @@
 #include <QTest>
 #include"file_test.h"
 #include"load_test.h"
+#include"net_test.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    LoadTest f;
-    FileTest t;
-    QTest::qExec(&t, argc, argv);
+    LoadTest l;
+    FileTest f;
+    NetTest  n;
+    QTest::qExec(&n, argc, argv);
 
-    return 0;
+    return a.exec();
 }
