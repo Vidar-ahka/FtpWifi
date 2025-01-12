82170c76a1111f0430a18dc79d1ab6f53e0198fb
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-01-12 19:46:19 +0300

    добавил класс abstractconnect

---
+++ include/net/abstractconnect.h
@@ -0,0 +1,24 @@
+#ifndef ABSTRACTCONNECT_H
+#define ABSTRACTCONNECT_H
+#include"QAbstractSocket"
+#include"memory"
+#include"file/fileabstract.h"
+class AbstractConnect : public QObject
+{
+    Q_OBJECT
+public:
+    AbstractConnect() = default;
+    AbstractConnect(std::unique_ptr<QAbstractSocket> socket)
+    {
+        this->socket = std::move(socket);
+    }
+    virtual QByteArray read() = 0;
+    virtual quint64    send(std::shared_ptr<AbstractFile> file) = 0;
+protected:
+    std::unique_ptr<QAbstractSocket> socket;
+signals:
+    void signalread(int id_message,QByteArray byte);
+
+};
+
+#endif
