8c97730b52dc7392c6ecb9919c17e2d0a9a6364b
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-06-27 14:28:18 +0300

    Добавил класс JsonFile в include/file/jsonfile.h
    -класс наследуеться от QJsonObject
    -написан конструктор принимающий QString path_file

---
+++ include/file/jsonfile.h
@@ -0,0 +1,17 @@
+#ifndef JSONFILE_H
+#define JSONFILE_H
+#include"QJsonDocument"
+#include"QJsonObject"
+
+class JsonFile : QJsonObject
+{
+public:
+    JsonFile(QString path);
+
+private:
+
+}
+
+
+#endif
+
