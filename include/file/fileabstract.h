2b6abf4d02a5096d4df4d44093042a5392f1292c
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-06-28 19:33:41 +0300

    refactor(file): удаление констурктора AbstractFile
    Удаленно:
     - Констуктор AbstractFile(QString path)
     - Переменная QString path
    
    Причина:
     - Переоброзование AbstractFile в IFileReader
    
    Refactor #1

--- include/file/fileabstract.h
+++ include/file/fileabstract.h
@@ -8,14 +8,12 @@ class AbstractFile
 
 public:
     AbstractFile() = default;
-    AbstractFile(const QString _path):path(_path){}
     virtual QByteArray read( const quint64 read_size) = 0;
     virtual QByteArray readall() = 0;
     virtual QString    getFileInfo()=0;
     virtual bool isOpen() = 0;
     virtual ~AbstractFile() = default;
 protected:
-    QString path;
 
 
 };
