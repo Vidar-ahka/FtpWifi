dd1c527412e641436ddb35e56bcd231d89f08072
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-06-28 19:55:35 +0300

    Rename(file):rename AbstractFile на IFileReader
    Изменино
     -название AbstracttFile на IFileReader
     -класс переформатирован из абстрактного класса в интерфейсЖ
    Rename #1

---
+++ include/file/ifilereader.h
@@ -0,0 +1,17 @@
+#ifndef  FILEABSTRACT_H
+#define  FILEABSTRACT_H
+#include"QString"
+#include"QObject"
+#include"functional"
+class IFileReader
+{
+
+public:
+    IFileReader() = default;
+    virtual QByteArray read( const quint64 read_size) = 0;
+    virtual QByteArray readall() = 0;
+    virtual QString    getFileInfo()=0;
+    virtual bool isOpen() = 0;
+    virtual ~AbstractFile() = default;
+};
+#endif
