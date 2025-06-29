32ad60cd9342a77cd200c0292781e96ca0680666
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-06-29 16:27:44 +0300

    modified(net):изменил базоый класс для класса Load
    Изменения:
    -iLoad теперь является  базаовым клаасом для ILoad вместо AbstractLoad
    -Добавленна переменная QString path для указания пути к загурзки
    
    modified #6

--- include/load/load.h
+++ include/load/load.h
@@ -1,9 +1,9 @@
 #ifndef LOAD_H
 #define LOAD_H
 #include"QFile"
-#include"Abstractload.h"
+#include"iload.h"
 #include"QDebug"
-class Load : public  AbstractLoad
+class Load : public ILoad
 {
 public:
     Load();
@@ -14,16 +14,20 @@ public:
 
     Load & operator = (const Load & load);
     Load & operator = (Load && load);
+
     quint64 append(const QByteArray  byte)override;
     bool isOpen()      override;
     bool isCompleted() override;
+
     virtual ~Load();
+
 private:
     void Move(Load & load);
     bool create_file(QString path);
     std::shared_ptr<QFile> file;
     quint64 exsize;
     quint64 cursize;
+    QString path;
 
 };
 
