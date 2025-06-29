bb84728614cd496da2bd42cbc7a55f4ea086805a
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-06-29 15:03:06 +0300

    modified(main): удаленн тестовый код
    modified #4

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -1,17 +1,12 @@
 #include "main/mainwindow.h"
 #include "ui_mainwindow.h"
-#include"file/file.h"
+
 #include"iostream"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    QFile file("C:\\zakazs\\thermal.c");
-    if(file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        std::cout<<file.readAll().toStdString();
 
-    }
     ui->setupUi(this);
 }
 
