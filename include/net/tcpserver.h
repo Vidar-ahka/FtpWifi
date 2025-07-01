498e9e0598ba5224e156e868e051fb93f98bfea3
Vidar-ahka <tamerlan_akaev15@mail.ru>
2025-07-01 20:41:59 +0300

    modified(net):создание DataReceiver в TcpServer
    Добаленно:
    -метод incommingConncetion
    -хэш - таблица для хранения DataReceiver

--- include/net/tcpserver.h
+++ include/net/tcpserver.h
@@ -4,17 +4,23 @@
 #include"QTcpServer"
 #include"iserver.h"
 #include"memory"
+#include"QTcpSocket"
+#include"datareceiver.h"
+#include"QHash"
 class TcpServer :  public IServer , public QTcpServer
 {
 public:
 
     TcpServer() = default;
     TcpServer(QString addres , quint16 port);
+    void incomingConnection(qintptr socektDescriptor);
+
+
+
+
 
 private:
-
-private slots:
-};
+    QHash<qintptr,std::shared_ptr<DataReceiver>> hash_dr;
 
 
 #endif
--- src/net/tcpserver.cpp
+++ src/net/tcpserver.cpp
@@ -3,8 +3,23 @@
 TcpServer::TcpServer(QString addres , quint16 port)
 {
     server = std::make_shared<QTcpServer>();
-    if(server->listen(QHostAddress(addres),port))
+    if(!server->listen(QHostAddress(addres),port))
     {
-
+        return;
     }
 }
+
+void TcpServer::incomingConnection(qintptr socketDescriptor)
+{
+    std::shared_ptr<QTcpSocket> socket = std::make_shared<QTcpSocket>();
+    socket->setSocketDescriptor(socketDescriptor);
+
+    std::shared_ptr<DataReceiver> datareceiver = std::make_shared<DataReceiver>(socket);
+    hash_dr[socketDescriptor] = datareceiver;
+
+
+
+}
+
+
+
